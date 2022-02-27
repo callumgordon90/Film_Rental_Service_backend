805fe9f0baf2f360c2b3b1f59be535e07f2fa570
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-02-27 16:46:34 +0100

    finished index.js . Port defined and dependencies and middleware called

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const db = requre ("./db");
+const app = express();
+const port = process.env.PORT || 3006;
+const router = require ("./router");
+const cors = require ('cors');
+
+//Middleware:
+app.use(cors());
+app.use(express.json());
+app.use(router);
+db
+.then(() =>{
+    app.listen(port, ()=> console.log (`Server is listening on http://localhost:${port}`));
+
+})
+.catch((err) => console.log (err.message) );
