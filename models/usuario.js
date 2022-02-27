9031cf77969c21570d87573f589b9e7a334d5a84
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-02-27 23:17:51 +0100

    added the models

---
+++ models/usuario.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Usuario extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasMany(models.Order, {
+        foreignKey: 'usuarioId'
+      });
+      this.hasOne(models.Payment, {
+        foreignKey: 'usuarioId'
+      });
+    }
+  };
+  Usuario.init({
+    name: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    surname: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    nickname: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Usuario',
+  });
+  return Usuario;
+};
