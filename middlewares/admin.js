951c814ea1c60d1df5d1cfff2581ed3ad8653195
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-02-27 17:42:46 +0100

    added admin js script

---
+++ middlewares/admin.js
@@ -0,0 +1,31 @@
+const jwt = require ('jsonwebtoken');
+const secret = "this is the json web token secret";
+
+const admin = (req, res, next) => {
+    try {
+        if (!req.headers.authorization){
+            return new Error ("You are not authorised");
+        }
+
+        let token = req.headers.authorization.split('')[1];
+
+        let auth = jwt.verify(token, secret);
+
+        //console.log(auth);
+
+        if((auth.userId != req.body.idUser) && (auth.isAdmin == req.body.isAdmin)){
+
+            //console.log(req.body.idUser);
+            throw new Error ("You do not have permission to perform this action");
+        }
+        return next ();
+
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+
+}
+
+module.export = admin;
