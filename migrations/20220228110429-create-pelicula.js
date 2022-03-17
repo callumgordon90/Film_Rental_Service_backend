8e2c575c0516a1a57fdf07a139ca059f0fc5aeef
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-03-17 10:39:39 +0100

    autoincrement false

--- migrations/20220228110429-create-pelicula.js
+++ migrations/20220228110429-create-pelicula.js
@@ -4,7 +4,7 @@ module.exports = {
     await queryInterface.createTable('Peliculas', {
       id: {
         allowNull: false,
-        autoIncrement: true,
+        autoIncrement: false,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
